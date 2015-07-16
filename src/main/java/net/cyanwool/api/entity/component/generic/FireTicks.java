0806d882858592b398559c3376998ec0dff5bf09
BeYkeRYkt <beykerykt@gmail.com>
2015-07-16 22:01:56 +0900

    add isBurned()

--- src/main/java/net/cyanwool/api/entity/component/generic/FireTicks.java
+++ src/main/java/net/cyanwool/api/entity/component/generic/FireTicks.java
@@ -4,6 +4,8 @@ import net.cyanwool.api.entity.component.Component;
 
 public interface FireTicks extends Component {
 
+	public boolean isBurned();
+	
 	public int getFireTicks();
 
 	public void setFireTicks(int ticks);
