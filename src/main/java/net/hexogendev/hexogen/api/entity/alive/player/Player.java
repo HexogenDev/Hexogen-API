d0c5e2630be39af88de1af512b983e48b380962b
BeYkeRYkt <beykerykt@gmail.com>
2015-11-06 17:53:31 +0900

    Updated Player

--- src/main/java/net/hexogendev/hexogen/api/entity/alive/player/Player.java
+++ src/main/java/net/hexogendev/hexogen/api/entity/alive/player/Player.java
@@ -1,6 +1,10 @@
 package net.hexogendev.hexogen.api.entity.alive.player;
 
 import net.hexogendev.hexogen.api.network.PlayerNetwork;
+import net.hexogendev.hexogen.api.world.Particle;
+import net.hexogendev.hexogen.api.world.Position;
+import net.hexogendev.hexogen.api.world.effect.Effect;
+import net.hexogendev.hexogen.api.world.sounds.Sound;
 
 public interface Player extends OfflinePlayer {
 
@@ -8,4 +12,13 @@ public interface Player extends OfflinePlayer {
 
 	public void kick(String reason);
 
+	public void playSound(Position pos, String sound, float volume, float pitch);
+
+	public void playSound(Position pos, Sound sound, float volume, float pitch);
+
+	public void playEffect(Position pos, Effect effect, int data);
+	
+	public void playParticle(Position pos, Particle particle, int amount, int data);
+
+	public void setTime(long worldTime);
 }
