5c4e750e9db865946fc3631f152afbf1ff87a550
BeYkeRYkt <beykerykt@gmail.com>
2015-12-26 16:47:56 +0900

    Build: Add new version "Unknown"

--- src/main/java/net/hexogendev/hexogen/api/Build.java
+++ src/main/java/net/hexogendev/hexogen/api/Build.java
@@ -5,19 +5,19 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Build {
 
-	public static final String UNKNOWN = "unknown";
 	public static final int CURRENT_VERSION = VERSION_CODES.AIR;
 
 	private static final List<String> map;
 
 	static {
 		map = new CopyOnWriteArrayList<String>();
-		map.add(0, UNKNOWN);
+		map.add(VERSION_CODES.UNKNOWN, "Unknown");
 		map.add(VERSION_CODES.AIR, "Air");
 		map.add(VERSION_CODES.DIRT, "Dirt");
 	}
 
 	public static class VERSION_CODES {
+		public static int UNKNOWN = 0;
 		public static int AIR = 1;
 		public static int DIRT = 2;
 	}
@@ -25,7 +25,7 @@ public class Build {
 	public static String getNameForInt(int id) {
 		final String name = map.get(id);
 		if (name == null) {
-			return UNKNOWN;
+			return map.get(0);
 		}
 		return name;
 	}
